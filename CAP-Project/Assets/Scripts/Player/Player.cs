9db3ac1706b766a6c9762232d38c80bf4974b29e
FhLk <68800909+FhLk@users.noreply.github.com>
2023-10-31 10:48:55 +0700

    Functional Change color player Complete

--- CAP-Project/Assets/Scripts/Player/Player.cs
+++ CAP-Project/Assets/Scripts/Player/Player.cs
@@ -4,14 +4,49 @@ using UnityEngine;
 
 public class Player : BasePlayer
 {
-    // Start is called before the first frame update
-    void Start()
+    private string[] colors = { "RED", "GREEN", "YELLOW", "BLUE" };
+    private int currentColorIndex = 0;
+    public SpriteRenderer playerRenderer;
+
+    private void Start()
     {
+        // Get the SpriteRenderer component attached to the player object
+        playerRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ChangeColorLeft()
     {
-        
+        currentColorIndex = (currentColorIndex - 1 + colors.Length) % colors.Length;
+        ChangePlayerColor();
+    }
+
+    public void ChangeColorRight()
+    {
+        currentColorIndex = (currentColorIndex + 1) % colors.Length;
+        ChangePlayerColor();
+    }
+
+    private void ChangePlayerColor()
+    {
+        // You can assign colors to your player based on the currentColorIndex
+        string color = colors[currentColorIndex];
+        switch (color)
+        {
+            case "RED":
+                playerRenderer.color = Color.red;
+                break;
+            case "GREEN":
+                playerRenderer.color = Color.green;
+                break;
+            case "YELLOW":
+                playerRenderer.color = Color.yellow;
+                break;
+            case "BLUE":
+                playerRenderer.color = Color.blue;
+                break;
+            default:
+                playerRenderer.color = Color.white; // Default color
+                break;
+        }
     }
 }
